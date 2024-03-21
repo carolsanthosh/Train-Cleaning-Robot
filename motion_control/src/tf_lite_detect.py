78f3ec9833020691c5fae0eecaf35b6561960355
Yukiyt778 <135519587+Yukiyt778@users.noreply.github.com>
2024-03-21 11:14:44 +0000

    Update tf_lite_detect.py

--- motion_control/src/tf_lite_detect.py
+++ motion_control/src/tf_lite_detect.py
@@ -5,7 +5,7 @@ import sys
 import glob
 import random
 import importlib.util
-import tflite_runtime.interpreter import Interpreter
+from tflite_runtime.interpreter import Interpreter
 
 lblpath = 'custom_model_lite/labelmap.txt'
 modelpath = 'custom_model_lite/detect.tflite'
